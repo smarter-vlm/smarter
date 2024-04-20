6b7dda6241f908c7dbaeea2ab9530b27f42dbb23
droberts308 <droberts308@gatech.edu>
2024-04-20 19:06:50 -0400

    Float attrib not found for im now

--- main_reasoner.py
+++ main_reasoner.py
@@ -123,7 +123,7 @@ def train(args, dataloader, im_backbone):
         model.train()
         tot_loss = 0.0
         for i, (im, q, _, a, av, pids) in tqdm(enumerate(train_loader)):
-            im = im.float()
+            # im = im.float()
             im = im.to(device)
             q = q.cuda()
             a = a.cuda()
