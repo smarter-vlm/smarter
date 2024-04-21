82ee2507c1dfed4239820dc16c6e30e41a88c601
droberts308 <droberts308@gatech.edu>
2024-04-21 09:56:23 -0400

    Test new torch int type branch

--- test_dinov2.py
+++ test_dinov2.py
@@ -1,18 +1,20 @@
-from transformers import AutoImageProcessor, Dinov2Model
+# from transformers import AutoImageProcessor, Dinov2Model
+# import torch
+# from datasets import load_dataset
+
+# dataset = load_dataset("huggingface/cats-image")
+# image = dataset["test"]["image"][0]
+
+# image_processor = AutoImageProcessor.from_pretrained("facebook/dinov2-base")
+# model = Dinov2Model.from_pretrained("facebook/dinov2-base")
+
+# inputs = image_processor(image, return_tensors="pt")
+
+# with torch.no_grad():
+#     outputs = model(**inputs)
+
+# last_hidden_states = outputs.last_hidden_state
+# list(last_hidden_states.shape)
 import torch
-from datasets import load_dataset
-
-dataset = load_dataset("huggingface/cats-image")
-image = dataset["test"]["image"][0]
-
-image_processor = AutoImageProcessor.from_pretrained("facebook/dinov2-base")
-model = Dinov2Model.from_pretrained("facebook/dinov2-base")
-
-inputs = image_processor(image, return_tensors="pt")
-
-with torch.no_grad():
-    outputs = model(**inputs)
-
-last_hidden_states = outputs.last_hidden_state
-list(last_hidden_states.shape)
-
+a = torch.tensor(1,dtype=torch.int)
+print(a.int())
