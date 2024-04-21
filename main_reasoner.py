4bdd7c601be91113b04678f63459e28bc67ed2db
Denisa Roberts <denisa.roberts@denisaroberts.me>
2024-04-21 17:19:00 -0400

    save

--- main_reasoner.py
+++ main_reasoner.py
@@ -473,7 +473,7 @@ if __name__ == "__main__":
     )
 
     args.preprocess = preprocess
-    print("preprocess now", preprocess)
+    print("preprocess now is **********************************", preprocess)
 
     train_loader = get_data_loader(
         args,
