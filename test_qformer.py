726a3cc1a9aa55927d6a9d42a145ee7a91a301bc
Denisa Roberts <denisa.roberts@denisaroberts.me>
2024-04-21 16:00:10 -0400

    Save again

--- test_qformer.py
+++ test_qformer.py
@@ -1,1 +1,21 @@
 # https://github.com/Zhudongsheng75/VisLingInstruct/blob/main/mmlm_vicuna.py
+import logging
+import string
+from packaging import version
+
+import torch
+from torch.cuda.amp import autocast as autocast
+import torch.nn as nn
+
+import transformers
+
+from mmlm.common.registry import registry
+from mmlm.models.blip2 import Blip2Base, disabled_train
+
+# not installing here on mac m1
+
+# note base in ice is python 3.12
+# cant 'f
+# pip install salesforce-lavis
+
+# will try with conda python version
