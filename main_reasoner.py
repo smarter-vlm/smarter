5299e49b3382b4ede2531e2a877a32a0b89372e8
Denisa Roberts <denisa.roberts@denisaroberts.me>
2024-04-21 17:22:20 -0400

    im deb

--- main_reasoner.py
+++ main_reasoner.py
@@ -125,7 +125,9 @@ def train(args, dataloader, im_backbone):
         model.train()
         tot_loss = 0.0
         for i, (im, q, _, a, av, pids) in tqdm(enumerate(train_loader)):
-            im = im.float()
+
+            # im = im.float()
+            print("Im now*************:", im)
             im = im.to(device)
             q = q.cuda()
             a = a.cuda()
