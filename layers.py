91c8a11d9ee1a2a4ae6d48fb4cc89684c6167000
Denisa Roberts <denisa.roberts@denisaroberts.me>
2024-04-23 09:57:25 -0400

    Add avg pooling on crossattn output

--- layers.py
+++ layers.py
@@ -49,11 +49,11 @@ class QFLayer(nn.Module):
 class QFIntermediate(nn.Module):
     def __init__(self):
         super().__init__()
-        self.dense = nn.Linear(896, 256)  # TODO DR shapes/hidden sizes
+        self.dense = nn.Linear(768, 256)  # TODO DR shapes/hidden sizes
         self.intermediate_act_fn = nn.GELU()
         self.layer_norm = nn.LayerNorm(256, eps=1e-12)
         self.dropout = nn.Dropout(0.1)
-        self.dense_final = nn.Linear(256, 256)
+        self.dense_final = nn.Linear(256, 768)
 
     def forward(self, hidden_states: torch.Tensor) -> torch.Tensor:
         x = self.dense(hidden_states)
@@ -61,8 +61,8 @@ class QFIntermediate(nn.Module):
 
         x = self.dense_final(x)
         x = self.dropout(x)
-        # x = self.layer_norm(hidden_states + x) # TODO skip the residual for now
-        x = self.layer_norm(x)
+        x = self.layer_norm(hidden_states + x) 
+        # x = self.layer_norm(x)
         return x
 
 
