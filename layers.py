57176fcf7ab699d342e38102fdaf026188040f9b
Denisa Roberts <denisa.roberts@denisaroberts.me>
2024-04-23 08:34:56 -0400

    First add of crossattention between text and vision

--- layers.py
+++ layers.py
@@ -19,19 +19,23 @@ class QFLayer(nn.Module):
         self.intermediate = QFIntermediate()
         self.mha = QFAttentionMH()
         # TODO DR add a num heads arg
+        self.crossattention = QFAttentionMH(num_attention_heads=1, hidden_size=768, encoder_hidden_size=128, max_position_embeddings=110, is_cross_attention=True)
 
     def forward(self, im_repr, q_repr):
         # q_repr is siglip encoding of the text sequence with max len 110
         q_attn = self.mha(q_repr).mean(1) # TODO DR -this can also be concat of all seq token repr
 
         # for now concat all heads together
-        print("self attn output shape ", q_attn.shape) #B, 896
-        print("what is the fused vision rep shape ", im_repr.shape) # B, 128; this is projected fused
+        print("self attn text output shape ", q_attn.shape) #B, 896
+        print("what is the project fused vision rep shape ", im_repr.shape) # B, 128; this is projected fused
 
-        x = torch.cat(
-            [im_repr, q_attn], dim=1
-        )  # STOP GAP DR; TODO here is cross attn
+        # x = torch.cat(
+        #     [im_repr, q_attn], dim=1
+        # )  # STOP GAP DR; TODO here is cross attn
 
+        x = self.crossattention(q_attn, im_repr)
+
+        print("\nWhat is the output shape after cross attn with mh self attn on siglip encoded text queries and projected fused vision encoded key and vals", x)
 
         x = self.intermediate(x)
         return x
