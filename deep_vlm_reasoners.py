dfc5df475ed92708c5084bcf298aa52c4d67e2ce
Denisa Roberts <denisa.roberts@denisaroberts.me>
2024-04-23 10:17:37 -0400

    Add args class attribute

--- deep_vlm_reasoners.py
+++ deep_vlm_reasoners.py
@@ -26,6 +26,8 @@ class Puzzle_Net(nn.Module):
         with open(vocab_path, "rb") as f:
             self.vocab = pickle.load(f)
 
+        self.args = args
+
         self.num_opts = 5
         self.out_dim = args.repr_size
         self.h_sz = 256
@@ -350,7 +352,7 @@ class Puzzle_Net(nn.Module):
         im_repr = self.encode_image(im.float(), puzzle_ids).float()
 
         # qv_repr = self.qv_fusion(torch.cat([im_repr, q_repr], dim=1))
-        if self.args.qf_layer:
+        if args.qf_layer:
             qf_out = self.qf(im_repr, q_repr)
             qv_repr = self.qv_fusion(self.c(im_repr, q_repr, qf_out))
         else:
