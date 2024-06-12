5173bbde3563a08987a46da2441a5fe7040c2156
Denisa Roberts <denisa.roberts@denisaroberts.me>
2024-06-12 12:07:48 -0400

    Try hf option for vjepa

--- try_vjepa.py
+++ try_vjepa.py
@@ -1,1 +1,12 @@
 # vjepa large
+# no official hf; need to install jepa folder 
+# siglip conda env
+# can't install one package on M1 - decoder 
+
+# https://huggingface.co/nielsr/vit-large-patch16-v-jepa
+
+from src.models.vision_transformer import VisionTransformer
+
+model = VisionTransformer.from_pretrained("nielsr/vit-large-patch16-v-jepa")
+print(model)
+# try from FB directly https://github.com/facebookresearch/jepa 
