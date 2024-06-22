d94053976c10d5ec90eda10b03925e8357a7cdd1
Denisa Roberts <denisa.roberts@denisaroberts.me>
2024-06-22 10:02:03 -0400

    Ablation on qf intermediate size

--- layers.py
+++ layers.py
@@ -56,17 +56,21 @@ class QFLayer(nn.Module):
 class QFIntermediate(nn.Module):
     def __init__(self, args):
         super().__init__()
-        self.dense = nn.Linear(768, args.h_sz)
+        # self.dense = nn.Linear(768, args.h_sz)
+        self.dense = nn.Linear(768, 768)
         self.intermediate_act_fn = nn.GELU()
         self.layer_norm = nn.LayerNorm(768, eps=args.ln_eps)
         self.dropout = nn.Dropout(args.pdrop)
-        self.dense_final = nn.Linear(args.h_sz, 768)
+        # self.dense_final = nn.Linear(args.h_sz, 768)
+        self.dense_final = nn.Linear(768, 768)
+
 
     def forward(self, hidden_states: torch.Tensor) -> torch.Tensor:
         x = self.dense(hidden_states)
         x = self.intermediate_act_fn(x)
 
         x = self.dense_final(x)
+        print(self.dense)
         x = self.dropout(x)
         x = self.layer_norm(hidden_states + x)
         return x
